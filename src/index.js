be471acd3f419240a51bda0f9abf024702d196af
Charlie Stopek <cstopek@hotmail.com>
2017-05-12 10:40:17 -0400

    code challange pencils down

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
   // your code here!
+
+  $('#photo-form').on('submit', function(event){
+      event.preventDefault();
+      var image = $("#image_url").val()
+      var caption = $("#caption").val()
+      console.log('here', image, caption)
+
+      // var picture = `<li><image><source src="${image}"></image></li>`
+      var picture =`<img src="${image}" >`
+
+
+        $('#photo-list').append(picture)
+    })
+
 })
